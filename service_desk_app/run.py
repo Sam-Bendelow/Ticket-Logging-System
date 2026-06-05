89e3e957f4d219b91c48abbee3d6ecb25dbb344e
Bendelow <sam.bendelow@coxautoinc.com>
2026-06-05 18:05:09 +0100

    Added database initialisation for deployment

--- service_desk_app/run.py
+++ service_desk_app/run.py
@@ -4,6 +4,12 @@ import os
 
 migrate = Migrate(application, db)
 
+# Create database tables
+from service_desk_app.app.models import User, Ticket, Note
+
+with application.app_context():
+    db-create_all()
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     application.run(host='0.0.0.0', port=port)
