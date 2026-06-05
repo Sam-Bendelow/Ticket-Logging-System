d09f1c0bc8c023843b081b89d66e167be6cd9820
Bendelow <sam.bendelow@coxautoinc.com>
2026-06-05 17:37:23 +0100

    Fix port configuration for Render

--- service_desk_app/run.py
+++ service_desk_app/run.py
@@ -1,7 +1,9 @@
 from service_desk_app.app import app as application, db
 from flask_migrate import Migrate
+import os
 
 migrate = Migrate(application, db)
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    application.run(host='0.0.0.0', port=port)
