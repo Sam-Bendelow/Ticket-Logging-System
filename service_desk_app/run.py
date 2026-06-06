0b09764eb374a42da9e38826b21e2eb7084ebf99
Bendelow <sam.bendelow@coxautoinc.com>
2026-06-06 22:05:32 +0100

    Database reset

--- service_desk_app/run.py
+++ service_desk_app/run.py
@@ -8,6 +8,7 @@ migrate = Migrate(application, db)
 from service_desk_app.app.models import User, Ticket, Note
 
 with application.app_context():
+    db.drop_all()
     db.create_all()
 
 if __name__ == '__main__':
